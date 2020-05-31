3e3de93f5741fe922305d05da44022d2a943253f
jason9829 <jason9829@live.com>
2020-05-31 14:06:08 +0800

    Fix indentation error.

--- main.py
+++ main.py
@@ -5,19 +5,19 @@ import myGlobal
 import jobstreet as jb
 
     #userInput = input("Please enter the key word to search")
-    userInput = "Manager"
-    soup = scraper.getBeautifulSoupInHtml(jb.getJobStreetUrl(userInput))
-    data = jb.getJobSoupFromJobStreet(soup)
+userInput = "Manager"
+soup = scraper.getBeautifulSoupInHtml(jb.getJobStreetUrl(userInput))
+data = jb.getJobSoupFromJobStreet(soup)
     #print(data)
 
-    i = 0
-    temp = len(data.jobTitle)
-    while temp != 0:
-        print('\nJob Title: ' + data.jobTitle[i].contents[0])
-        print('Company Name: ' + data.companyName[i].text.replace('\n', ''))
-        print('Job Link: ' + data.jobLink[i].attrs['href'])
-        print('Job Location: ' + data.jobLocation[i].next.getText())
-        print('Job Posted: ' + data.jobPost[i].contents[0].lstrip())
-        iterationNum = myGlobal.getIterationValue(i)
-        i = i + iterationNum
-        temp = temp - iterationNum
+i = 0
+temp = len(data.jobTitle)
+while temp != 0:
+    print('\nJob Title: ' + data.jobTitle[i].contents[0])
+    print('Company Name: ' + data.companyName[i].text.replace('\n', ''))
+    print('Job Link: ' + data.jobLink[i].attrs['href'])
+    print('Job Location: ' + data.jobLocation[i].next.getText())
+    print('Job Posted: ' + data.jobPost[i].contents[0].lstrip())
+    iterationNum = myGlobal.getIterationValue(i)
+    i = i + iterationNum
+    temp = temp - iterationNum
