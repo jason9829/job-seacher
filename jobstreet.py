7086a5d30c1dac90531ee15cbcdbcf9b7b831b57
jason9829 <jason9829@live.com>
2020-05-31 13:54:06 +0800

    Add Travis CI and function
    1. Change variable name.
    2. Add function to get soup for JobStreet.com (job title, company name, job link, job location and job posted time).
    3. Create a structure named JobStreetToken to store job data.
    4. Add Travis CI to run main.py and scraper.py.

--- jobstreet.py
+++ jobstreet.py
@@ -1,8 +1,29 @@
 
+class JobStreetToken:
+    def __init__(self):
+        self.jobTitle = None
+        self.companyName = None
+        self.jobLocation = None
+        self.jobLink = None
+        self.jobPost = None
 
-# Desc: Create the url for the specific search term for Job Street
+
+# Desc: Create the URL for the specific search term for Job Street
 # Param: Search term (job)
-# Retval: Url with the search term
+# Retval: URL with the search term
 def getJobStreetUrl(searchTerm):
     searchTerm = searchTerm.replace(" ", "+")
     return "https://www.jobstreet.com.my/en/job-search/job-vacancy.php?ojs=10&key=" + searchTerm
+
+
+# Desc: Get beautiful soup structures for Job Street data (one page)
+# Param: Scraped Beautiful Soup
+# Retval: Job Street data
+def getJobSoupFromJobStreet(soup):
+    soupList = JobStreetToken()
+    soupList.jobTitle = soup.find_all('h2')
+    soupList.companyName = soup.find_all('h3')
+    soupList.jobLink = soup.findAll("a", {"class": "position-title-link"})
+    soupList.jobLocation = soup.findAll("i", {"class": "icon icon-location"})
+    soupList.jobPost = soup.findAll("span", {"class": "job-date-text text-muted"})
+    return soupList
--- main.py
+++ main.py
@@ -6,21 +6,19 @@ import jobstreet as jb
 
 while True:
     userInput = input("Please enter the key word to search")
+    #userInput = "Manager"
     soup = scraper.getBeautifulSoupInHtml(jb.getJobStreetUrl(userInput))
+    data = jb.getJobSoupFromJobStreet(soup)
+    #print(data)
 
-    jobTitle = soup.find_all('h2')
-    companyTitle = soup.find_all('h3')
-    jobLink = soup.findAll("a", {"class": "position-title-link"})
-    jobLocation = soup.findAll("i", {"class": "icon icon-location"})
-    jobPostTime = soup.findAll("span", {"class": "job-date-text text-muted"})
     i = 0
-    temp = len(jobTitle)
+    temp = len(data.jobTitle)
     while temp != 0:
-        print('\nJob Title: ' + jobTitle[i].contents[0])
-        print('Company Title: ' + companyTitle[i].text.replace('\n', ''))
-        print('Job Link: ' + jobLink[i].attrs['href'])
-        print('Job Location: ' + jobLocation[i].next.getText())
-        print('Post time: ' + jobPostTime[i].contents[0].lstrip())
+        print('\nJob Title: ' + data.jobTitle[i].contents[0])
+        print('Company Name: ' + data.companyName[i].text.replace('\n', ''))
+        print('Job Link: ' + data.jobLink[i].attrs['href'])
+        print('Job Location: ' + data.jobLocation[i].next.getText())
+        print('Job Posted: ' + data.jobPost[i].contents[0].lstrip())
         iterationNum = myGlobal.getIterationValue(i)
         i = i + iterationNum
         temp = temp - iterationNum
