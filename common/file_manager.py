0642904fea4999f18f616bfd03663f11cfb81720
jxwleong <jason.xie.wei.leong@gmail.com>
2021-02-01 18:09:39 +0800

    Remove the class for file_manager because there's no attributes to be bind with the class

--- common/file_manager.py
+++ common/file_manager.py
@@ -1,11 +1,10 @@
 import os
 
-class FileManager():
-    @classmethod
-    def is_path_exist(self, path):
-        return os.path.exists(path)
 
-    @classmethod
-    def create_dir_if_not_exist(self, path):
-        if not self.is_path_exist(path):
-            os.mkdir(path)
+def is_path_exist(path):
+    return os.path.exists(path)
+
+
+def create_dir_if_not_exist(path):
+    if not is_path_exist(path):
+        os.mkdir(path)
--- test/test_file_manager.py
+++ test/test_file_manager.py
@@ -5,15 +5,15 @@ import sys
 ROOT_DIR = os.path.normpath(os.path.join(os.path.abspath(__file__), "..", ".."))
 sys.path.insert(0, ROOT_DIR)
 
-from common.file_manager import FileManager
+import common.file_manager as FM
 
 
 class TestFileManager(unittest.TestCase):
     def test_is_path_exist_given_root_dir_expect_true(self):
-        self.assertEqual(True, FileManager.is_path_exist(ROOT_DIR))
+        self.assertEqual(True, FM.is_path_exist(ROOT_DIR))
 
     def test_is_path_exist_given_invalid_dir_expect_false(self):
-        self.assertEqual(False, FileManager.is_path_exist(os.path.join(ROOT_DIR, 'invalid')))
+        self.assertEqual(False, FM.is_path_exist(os.path.join(ROOT_DIR, 'invalid')))
 
 
 if __name__ == '__main__':
